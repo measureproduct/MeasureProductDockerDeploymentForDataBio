4761ef7c1ff26534e319e6420b87a554008528f0
ambpro <amin.boudeffa@softeam.fr>
2019-10-11 18:21:39 +0200

    Externalize Dockerfile's properties configuration

--- measure-platform/startup.sh
+++ measure-platform/startup.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-ZIP_URL="https://github.com/measureproduct/DataSourceCatalog/releases/download/v1.0.0/DataSourceCatalog.zip"
-ZIP_FILE="DataSourceCatalog.zip"
-CATALOGUE_APPLICATION="/home/PackagedPlatform/storage/applications"
-CATALOGUE_MEASURE="/home/PackagedPlatform/storage/measures"
+ZIP_CATALOGUE_URL="https://github.com/measureproduct/DataSourceCatalog/releases/download/v1.0.0/DataSourceCatalog.zip"
+ZIP_CATALOGUE_FILE="DataSourceCatalog.zip"
+MEASUREPLATFORM_STORAGE_APPLICATION="/home/PackagedPlatform/storage/applications"
+MEASUREPLATFORM_STORAGE_MEASURE="/home/PackagedPlatform/storage/measures"
 
-wget $ZIP_URL
-unzip ./$ZIP_FILE -d ./tmp
+wget ${ZIP_CATALOGUE_URL}
+unzip ./${ZIP_CATALOGUE_FILE} -d ./tmp
 
 find ./tmp/BusinessApplication -name '*.zip'| while read zipFile ; do
     zipName="$(basename "${zipFile}" .zip)"
-    mkdir -p "${CATALOGUE_APPLICATION}/${zipName}"
-    unzip "${zipFile}" -d  "${CATALOGUE_APPLICATION}/${zipName}"
+    mkdir -p "${MEASUREPLATFORM_STORAGE_APPLICATION}/${zipName}"
+    unzip "${zipFile}" -d  "${MEASUREPLATFORM_STORAGE_APPLICATION}/${zipName}"
 done
 
 find ./tmp/Measure -name '*.zip'| while read zipFile ; do
     zipName="$(basename "${zipFile}" .zip)"
-    mkdir -p "${CATALOGUE_MEASURE}/${zipName}"
-    unzip "${zipFile}" -d  "${CATALOGUE_MEASURE}/${zipName}"
+    mkdir -p "${MEASUREPLATFORM_STORAGE_MEASURE}/${zipName}"
+    unzip "${zipFile}" -d  "${MEASUREPLATFORM_STORAGE_MEASURE}/${zipName}"
 done
 
 rm -r ./tmp
 
-rm -r $ZIP_FILE
+rm -r ${ZIP_CATALOGUE_FILE}
 
 
