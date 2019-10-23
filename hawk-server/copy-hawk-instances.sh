9241bdd48c351f7746f7219ca6739261a2fc7763
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-10-23 11:08:35 +0200

    customized volum from exterior of Docker image

--- hawk-server/copy-hawk-instances.sh
+++ hawk-server/copy-hawk-instances.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /home/hawk-instances/* /home/hawk-server-nogpl_20190202170100/configuration/
+cp ${HAWK_INSTANCES_CONTAINER_PATH}/* /home/hawk-server-nogpl_20190202170100/configuration/
 
 
